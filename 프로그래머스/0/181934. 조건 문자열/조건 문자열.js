da2dddfe3e6a1e55fa2be94b573f8a64dda3588c
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-01-18 18:17:04 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,8 @@
+function solution(ineq, eq, n, m) {
+
+    if (ineq === '<' && n < m) return 1
+    if (ineq === '>' && n > m) return 1
+    if (eq === '!' && n === m) return 1
+    if (eq === '=' && n === m) return 1
+    return 0
+}
