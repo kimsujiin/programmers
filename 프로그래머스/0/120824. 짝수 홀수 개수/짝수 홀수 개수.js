4f66f5f6b355df93460f19d89dd163c0349ac678
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-01-25 14:28:53 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -2,13 +2,15 @@ function solution(num_list) {
     let odd = 0;
     let even = 0;
     
-    for(let i =0 ; i<num_list.length;i++){
-        if(num_list[i] % 2 === 0){
+   
+    num_list.forEach(num => {
+        if (num % 2 === 0) {
             even++;
-        }else{
+        } else {
             odd++;
         }
-    }
+    });
     
-    return [ even ,odd]
+    return [ even , odd ]
 }
+
