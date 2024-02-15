4b072a2575ca572fbdf4f592e9db2dc265df5c5b
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-02-15 18:28:22 +0900

    [level 0] Title: 모스부호 (1), Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    let result = letter.split(' ')
+    let answer = ''
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    
+    for(let i = 0; i< result.length; i++){
+        answer += morse[result[i]]
+    }
+
+    return answer
+}
