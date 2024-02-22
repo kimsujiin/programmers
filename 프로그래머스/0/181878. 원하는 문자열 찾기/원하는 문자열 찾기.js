d5d6c96e8f8c185aa3230dd39ee3cf8f5fd12ab2
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-02-22 18:08:26 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.13 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -0,0 +1,11 @@
+function solution(myString, pat) {
+    
+    myString = myString.toLowerCase();
+    pat = pat.toLowerCase();
+    
+    if(myString.includes(pat)){
+        return 1;
+    }else {
+        return 0
+    }
+}
