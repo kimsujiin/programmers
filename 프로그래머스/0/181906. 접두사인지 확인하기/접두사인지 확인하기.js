5c1c7e3a69a62b317e54030b985a07f54212a933
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-02-26 14:24:43 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,7 +1,3 @@
 function solution(my_string, is_prefix) {
-    if(my_string.startsWith(is_prefix)){
-        return 1
-    }else{
-        return 0
-    }
+    return my_string.split(is_prefix)[0] === "" ? 1 : 0;
 }
