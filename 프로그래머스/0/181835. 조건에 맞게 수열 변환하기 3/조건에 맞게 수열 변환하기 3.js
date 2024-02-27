1bc6cbb8282c1e5baaaf45e4ed1f6b4e7ffacdda
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-02-27 14:36:06 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 2.95 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,16 +1,3 @@
 function solution(arr, k) {
-    let result = []
-    
-    for(let i = 0; i<arr.length; i++){
-        
-        if(k % 2 ===1){
-         result.push(arr[i]*k)
-        }
-        else
-        {
-         result.push(arr[i]+k)
-        }
-    }
-            
-    return result
+  return arr.map((num) => (k % 2 !== 0 ? num * k : num + k));
 }
