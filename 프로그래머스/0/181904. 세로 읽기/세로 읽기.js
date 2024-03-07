6cc88daa3713f75b5dc92447a809c28346668de9
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-03-07 18:23:40 +0900

    [level 0] Title: 세로 읽기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -2,9 +2,10 @@ function solution(my_string, m, c) {
     let answer = '';
     
     for(let i = c-1; i< my_string.length; i+=m){
+        
         answer += my_string[i]
     }
     
-    
     return answer;
-}
+}                                                     
+
