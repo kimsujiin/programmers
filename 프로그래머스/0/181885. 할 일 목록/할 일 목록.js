b715d0a9e7b4b0dbc0e0b3977d8cc6ea5225e4da
Sujin <150323302+kimsujiin@users.noreply.github.com>
2024-03-07 13:53:07 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.js
@@ -0,0 +1,10 @@
+function solution(todo_list, finished) {
+    let unfinished=[];
+    
+    for(let i =0; i<todo_list.length; i++){
+        if(!finished[i]){
+            unfinished.push(todo_list[i])
+        }
+    }
+    return unfinished
+}
